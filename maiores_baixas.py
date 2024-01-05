64aad8952aec5278059aef50b72335b25a26901c
felipetlutfi <felipe.turolla.lutfi@gmail.com>
2024-01-05 14:38:12 -0300

    Update maiores_baixas.py

--- maiores_baixas.py
+++ maiores_baixas.py
@@ -26,13 +26,8 @@ def obter_dados():
             
             dados = json.loads(result_value)
 
-            x['resultPercentageValue'] = x['resultPercentageValue'].replace(',', '.')
-
             # Ordena os dados com base no valor de 'resultPercentageValue'
-            dados_ordenados = sorted(dados, key=lambda x: float(x['resultPercentageValue']))
-
-            # Replace comma with dot in the string
-            dados_ordenados = dados_ordenados.replace(',', '.')
+            dados_ordenados = sorted(dados, key=lambda x: float(x['resultPercentageValue'].replace(',', '.')))
 
             # Seleciona os três elementos com os menores valores
             indices_desejados = dados_ordenados[:3]
