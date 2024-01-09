9c5c8895b9fef261b3c737be224c2c7300a3dc1b
felipetlutfi <felipe.turolla.lutfi@gmail.com>
2024-01-09 12:31:47 -0300

    Update tesouro_americano.py

--- tesouro_americano.py
+++ tesouro_americano.py
@@ -1,31 +1,43 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-def get_us10y_price():
-    url = "https://www.cnbc.com/quotes/US10Y"
-    
+def get_us10y_price(url):
     response = requests.get(url)
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        span_last_price = soup.find('span', class_='QuoteStrip-lastPrice')
-        last_price = span_last_price.text.strip()
-        return last_price
+        td_element = soup.find('td', headers='view-field-bc-10year-table-column', class_='views-field views-field-field-bc-10year')
+        price = td_element.text.strip()
+        return price
     else:
         return None
 
-@app.route('/us10y_price', methods=['GET'])
-def us10y_price():
-    us10y_price_value = get_us10y_price()
+@app.route('/us10y_prices', methods=['GET'])
+def us10y_prices():
+    # Obter a cotação de hoje
+    url_today = "https://www.cnbc.com/quotes/US10Y"
+    us10y_price_today_value = get_us10y_price(url_today)
     
-    if us10y_price_value is not None:
-        data = {"US10Y_Price": us10y_price_value}
+    # Obter a data de ontem
+    yesterday = datetime.now() - timedelta(days=1)
+    formatted_date = yesterday.strftime("%Y%m%d")
+    
+    # Obter a cotação de ontem
+    url_yesterday = f"https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView?type=daily_treasury_yield_curve&field_tdr_date_value_month={formatted_date}"
+    us10y_price_yesterday_value = get_us10y_price(url_yesterday)
+    
+    if us10y_price_today_value is not None and us10y_price_yesterday_value is not None:
+        data = {
+            "US10Y_Price_Today": us10y_price_today_value,
+            "US10Y_Price_Yesterday": us10y_price_yesterday_value
+        }
         return jsonify(data)
     else:
-        return jsonify({"error": "Erro ao obter a cotação do Tesouro Americano de 10 anos"}), 500
+        return jsonify({"error": "Erro ao obter as cotações do Tesouro Americano de 10 anos"}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
